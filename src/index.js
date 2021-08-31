d9f8ffc34ca0e386b2e972c0ff22f9ecb9f6d1ce
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2021-08-31 19:46:47 +0200

    Improved config

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { robotsTxt, securityTxt } from "./other";
 
 export const ROOT_DOMAIN = "m4rc3l.de";
 export const MATRIX_CLIENT = `https://matrix.${ROOT_DOMAIN}`;
-export const MATRIX_FEDERATION = `matrix-federation.${ROOT_DOMAIN}:443`;
+export const MATRIX_FEDERATION = `matrix-federation-int.${ROOT_DOMAIN}:443`;
 
 const router = Router()
   .get("/robots.txt", robotsTxt)
