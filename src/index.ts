2d2b0209e6476b68d4117465f566bce228674179
juli06 <62934740+juli0604@users.noreply.github.com>
2022-01-28 21:58:52 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,11 @@ import { robotsTxt, securityTxt } from "./other";
 import { onlyDomain, onlyRootDomain, redirectRootDomain } from "./utils";
 import { matrixClient, matrixServer } from "./matrix";
 
-export const ROOT_DOMAIN = "m4rc3l.de";
+export const ROOT_DOMAIN = "admin-guide.com";
 export const MATRIX_CLIENT = `https://matrix.${ROOT_DOMAIN}`;
-export const MATRIX_FEDERATION = `matrix-federation.${ROOT_DOMAIN}:443`;
+export const MATRIX_FEDERATION = `matrix.${ROOT_DOMAIN}:443`;
 
 export const REDIRECT_DOMAINS = [
-  `www.${ROOT_DOMAIN}`,
-  "marcelcoding.ml",
-  "www.marcelcoding.ml",
-  "marcelcoding.tk",
-  "www.marcelcoding.tk",
 ];
 
 const router = Router()
